c68a41f6959add263c689a31655c4d037d7c7982
Juan Alejandro Dev <juan.a1ejo32@hotmail.com>
2023-11-03 20:52:54 -0500

    Update App.js

--- uniswap-widget/src/App.js
+++ uniswap-widget/src/App.js
@@ -6,10 +6,23 @@ const jsonRpcUrlMap = {
   1: ['https://eth-mainnet.g.alchemy.com/v2/E2HbvxalVEIwdFH1McL0tSkcBkpxD1aV']
 }
 
+const theme: Theme = {
+  primary: '#1F4A05',
+  secondary: '#5F7D52',
+  interactive: '#CBD6BA',
+  container: '#D9ECD9',
+  module: '#E9F7DF',
+  accent: '#8E8B78',
+  outline: '#CADDC2',
+  dialog: '#FFF',
+  fontFamily: 'Nunito',
+  borderRadius: 0.8,
+}
+
 function App() {
   return (
     <div className="Uniswap">
-    <SwapWidget jsonRpcUrlMap={jsonRpcUrlMap}/>
+    <SwapWidget jsonRpcUrlMap={jsonRpcUrlMap}  theme={theme}/>
   </div>
   );
 }
